0010be0163e731edf57acac106f46b27465bd1fa
Marco6666662 <1419699960@qq.com>
2023-12-08 22:34:35 -0500

    folder for monte carlo

---
+++ Montecarlo/main.py
@@ -0,0 +1,211 @@
+from tictactoe import TicTacToe
+from TTTPlayer import TTTPlayer
+from ultTTT import ultTTT
+from ultTTTplayer import ultTTTplayer
+from visual import Visual
+
+import dash
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import numpy as np
+
+
+def check_rows(board):
+    for row in board:
+        if len(set(row)) == 1:
+            return row[0]
+    return None
+
+def check_diagonals(board):
+    if len(set([board[i][i] for i in range(len(board))])) == 1:
+        return board[0][0]
+    if len(set([board[i][len(board)-i-1] for i in range(len(board))])) == 1:
+        return board[0][len(board)-1]
+    return None
+
+def check_win(board):
+    # Check rows and columns
+    for new_board in [board, np.transpose(board)]:
+        result = check_rows(new_board)
+        if result:
+            return result
+    # Check diagonals
+    return check_diagonals(board)
+
+def create_small_board_layout(small_board, winner=None):
+    if winner:
+        # If there's a winner, return a large "X" or "O"
+        return html.Div(winner, style={'width': '90px', 'height': '90px', 'fontSize': '4em', 'textAlign': 'center', 'border': '1px solid black'})
+    else:
+        # Otherwise, return the standard 3x3 grid
+        return html.Table(
+            # Each row in the small board
+            [html.Tr([
+                html.Td(
+                    small_board[i][j] if small_board[i][j] != " " else html.Div(' ', style={'minWidth': '30px', 'minHeight': '30px'}),
+                    style={'width': '30px', 'height': '30px', 'border': '1px solid black', 'textAlign': 'center'}
+                ) for j in range(3)
+            ]) for i in range(3)],
+            style={'borderCollapse': 'collapse', 'display': 'inline-block', 'margin': '2px'}
+        )
+
+def create_ultimate_board_layout(boards):
+    if boards.shape == (3, 3, 3, 3):
+        ultimate_board_layout = []
+        for row_of_boards in boards:
+            row_layout = []
+            for small_board in row_of_boards:
+                # Check if we have a winner for the small board
+                winner = None
+                # Generate the layout for the small board
+                cell_layout = create_small_board_layout(small_board, winner=winner)
+                row_layout.append(cell_layout)
+            # Append the row of small boards to the ultimate board layout
+            ultimate_board_layout.append(html.Div(row_layout, style={'display': 'flex', 'justifyContent': 'center'}))
+        return html.Div(ultimate_board_layout, style={'textAlign': 'center'})
+    else:
+        result_layout = []
+        for i in range(3):
+            row_layout = []
+            for j in range(3):
+                # Create a cell for each result
+                cell = html.Div(boards[i][j], style={
+                    'width': '90px', 'height': '90px', 'fontSize': '4em',
+                    'textAlign': 'center', 'border': '1px solid black', 'display': 'inline-block'
+                })
+                row_layout.append(cell)
+            result_layout.append(html.Div(row_layout, style={'display': 'flex', 'justifyContent': 'center'}))
+        return html.Div(result_layout, style={'textAlign': 'center'})
+    
+def eval(num_games):
+    
+    p1_win = 0
+    p2_win = 0
+    draw = 0
+
+    for game in range(num_games):
+        player1 = ultTTTplayer("player 1", "O")
+        player2 = ultTTTplayer("player 2", "X", monte_carlo=True)
+        UltTTT = ultTTT(player1, player2)
+        UltTTT.playGame()
+
+        winner = UltTTT.getUltWinner()
+
+        if winner == 0:
+            draw += 1
+        elif winner.name == "player 1":
+            p1_win += 1
+        elif winner.name == "player 2":
+            p2_win += 1
+
+        print(f'game: {game}')
+
+    p1_win_rate = round(p1_win / num_games * 100, 3)
+    p2_win_rate = round(p2_win / num_games * 100, 3)
+
+    print(f"Player 1 win {p1_win_rate} percent over {num_games} games")
+    print(f"Player 2 win {p2_win_rate} percent over {num_games} games")
+
+
+#RUN FOR ULTIMATE
+if __name__ == "__main__":
+    eval(100)
+#     player1 = ultTTTplayer("player 1", "O")
+#     player2 = ultTTTplayer("player 2", "X", monte_carlo=True)
+
+#     ultTTT = ultTTT(player1, player2)
+#     ultTTT.playGame()
+
+#     records = ultTTT.board_record
+
+#     results = ultTTT.result_board
+
+#     records = np.array(records)
+
+#     app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+#     # Initialize an empty 3x3x3 board
+#     initial_boards = np.full((3, 3, 3, 3), " ")
+
+#     app.layout = html.Div([
+#     html.H1("Ultimate Tic-Tac-Toe Board"),
+#     dcc.Store(id='board-state', data=initial_boards.tolist()),  # Store to hold board state
+#     dcc.Interval(id='interval-component', interval=1000, n_intervals=0),  # Update every second
+#     html.Div(id='ultimate-tic-tac-toe-board')
+# ])
+
+
+
+#     @app.callback(
+#     Output('board-state', 'data'),
+#     Input('interval-component', 'n_intervals')
+# )
+#     def update_board_state(n_intervals):
+#         #print(f"Interval: {n_intervals}")
+#         index = n_intervals % len(records) 
+#         board = records[index]  # Get the current board from the records
+#         #print(f"Board at index {index}: {board}")
+
+#         if n_intervals + 1 > len(records):
+#             return results.tolist()
+#         else:
+#             return board.tolist()  
+
+#     @app.callback(
+#     Output('ultimate-tic-tac-toe-board', 'children'),
+#     Input('board-state', 'data')
+# )
+#     def update_ultimate_board_layout(current_board_state):
+#         # Convert the current board state back to a 4D NumPy array
+#         boards = np.array(current_board_state, dtype=object)
+#         # Generate the board layout using the current board state
+#         board_layout = create_ultimate_board_layout(boards)
+#         return board_layout
+
+#     app.run_server(debug=True)
+
+
+
+# #RUN FOR REGULAR
+# you will want to see how the algorithm works for regular tictaetoe by using this
+#if __name__ == "__main__":
+#    player1 = TTTPlayer("player 1", "O")
+#    player2 = TTTPlayer("player 2", "X", monte_carlo=True)
+#    ttt = TicTacToe(player1, player2, True)
+#    ttt.playGame()
+
+    #visual.run_visual()
+
+    # p1_win = 0
+    # p2_win = 0
+    # draw = 0
+    # num_game = 100
+
+    # for game in range(num_game):
+    #     player1 = TTTPlayer("player 1", "O")
+    #     player2 = TTTPlayer("player 2", "X", monte_carlo=True)
+    #     ttt = TicTacToe(player1, player2, True)
+    #     ttt.playGame()
+
+    #     winner = ttt.getWinner()
+    #     if winner == 0:
+    #         draw += 1
+    #     elif winner.name == "player 1":
+    #         p1_win += 1
+    #     elif winner.name == "player 2":
+    #         p2_win += 1
+    #     #visual.run_visual()
+
+    # p1_win_rate = round(p1_win / num_game * 100, 3)
+    # p2_win_rate = round(p2_win / num_game * 100, 3)
+
+    # print(f"Player 1 win {p1_win_rate} percent over {num_game} games")
+    # print(f"Player 2 win {p2_win_rate} percent over {num_game} games")
+
+
+
+
+
+
+
---
+++ Montecarlo/tictactoe.py
@@ -0,0 +1,143 @@
+import numpy as np
+from TTTPlayer import TTTPlayer
+from typing import Final
+from visual import Visual
+
+class TicTacToe:
+
+    xSize: Final[int] = 3
+    ySize: Final[int] = 3
+
+    # The board is represented as a 3x3 matrix of ints
+    # empty represents an unplayed square
+    # 1 or 2 represents playerO and playerX respectively
+    # all other values are invalid
+
+
+    # constructs a TicTacToe game from the given players
+    # CHRIS: added a Visual parameter
+    def __init__(self, player1, player2, displayTurn):
+        self.turnNum = 0
+        self.board = np.full((3,3), " ")
+        self.playerO = player1
+        self.playerX = player2
+        self.displayTurn = displayTurn
+        # self.visual = visual
+
+    # Returns the string representation of the board
+    def __repr__(self) -> str:
+        if self.displayTurn:
+            return f"Turn: {self.turnNum}\n{self.board.__str__()}\n"
+        else:
+            return f"{self.board.__str__()}\n"
+
+    # Returns a list of tuples representing the possible moves to play
+    # i.e. the list of unplayed squares on the grid for basic tictactoe
+    def getPossibleMoves(self):
+        possibleMoves = []
+        for i in range(self.ySize):
+            for j in range(self.xSize):
+                if self.board[i,j] == " ":
+                    possibleMoves.append((i,j))
+        return possibleMoves
+
+    # Checks the rows to see if anybody has completed a line
+    def checkRows(self, board):
+        for row in board:
+            if len(set(row)) == 1:
+                return row[0]
+        return 0
+
+    # Checks the diagonals to see if anybody has completed a line 
+    def checkDiagonals(self, board):
+        if len(set([board[i][i] for i in range(len(board))])) == 1:
+            return board[0][0]
+        if len(set([board[i][len(board)-i-1] for i in range(len(board))])) == 1:
+            return board[0][len(board)-1]
+        return 0
+
+    # checks if somebody has won yet
+    # if they have, returns their symbol (e.g. "X" or "O")
+    # otherwise, returns 0
+    def checkWin(self):
+        for newBoard in [self.board, np.transpose(self.board)]:
+            result = self.checkRows(newBoard)
+            if result:
+                return result
+        return self.checkDiagonals(self.board)
+    
+    # Returns the winning Player object
+    # If nobody has won yet, returns 0
+    def getWinner(self) -> TTTPlayer:
+        result = self.checkWin()
+        if result == " " or result == 0:
+            return 0
+        elif result == "X":
+            return self.playerX
+        elif result == "O":
+            return self.playerO
+        else:
+            print(self, flush=True)
+            raise ValueError(f"Invalid value in grid: {result}")
+        
+    # Prompts a player to take a turn
+    # Checks if the turn is valid
+    # If the turn is valid, it will enact the requested placement on the board
+    def playerTakeTurn(self, player, playerSymbol, node=None, board=None, monte_carlo_on=False):
+        possibleMoves = self.getPossibleMoves()
+        turn = player.takeTurn(possibleMoves, node, board, monte_carlo_on)
+        if turn in possibleMoves:
+            self.board[turn] = playerSymbol
+            # self.visual.add_to_order(turn, playerSymbol)
+            #self.visual.add_to_order(turn, playerSymbol)
+        self.turnNum += 1
+        print("This is the tictactoe class: " + str(self.board))
+        print(self)
+
+        return turn
+
+
+    # Run a game to completion
+    # Prints the board state after each placement
+    # Announces the winner at the end
+    def playGame(self, monte_carlo_on=False):
+        # monte_carlo_on indicates if we are still running the monte carlo or it is real game
+        winner = 0
+        print(self.board)
+
+        turn_O = None
+
+        while True:
+            #turn_O = self.playerTakeTurn(self.playerO, "O")
+            turn_X = self.playerTakeTurn(self.playerX, "X", turn_O, self, monte_carlo_on)
+            winner = self.getWinner()
+            if winner != 0:
+                break
+            if " " not in self.board and winner == 0:
+                print("It's a draw!")
+                break
+            #turn_X = self.playerTakeTurn(self.playerX, "X", turn_O, self, monte_carlo_on)
+            turn_O = self.playerTakeTurn(self.playerO, "O")
+            winner = self.getWinner()
+            if winner != 0:
+                break
+            if " " not in self.board and winner == 0:
+                print("It's a draw!")
+                break
+        if winner != 0:
+            print(f"'{winner.getName()}' has won!")
+
+    # ADDED BY CHRIS FOR ULTIMATE VERSION
+    # Prompts a player to take a turn
+    # Checks if the turn is valid
+    # If the turn is valid, it will enact the requested placement on the board
+    def ultTakeTurn(self, player, playerSymbol, turn):
+        self.board[turn] = playerSymbol
+
+    #ADDED BY CHRIS FOR ULTIMATE VERSION
+    def checkDraw(self):
+        if " " not in self.board:
+            return True
+        else:
+            return False
+        
